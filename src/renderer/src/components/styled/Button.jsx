96a37e8a76242f60c9860d3fe6189bf83cf009ab
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-07-08 00:19:19 +0600

    basic design

--- src/renderer/src/components/styled/Button.jsx
+++ src/renderer/src/components/styled/Button.jsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-  padding: 10px;
   outline: 0;
   border: 0;
   cursor: pointer;
+  background-color: #545454;
+  color: white;
+  padding: 7px 1rem;
+  font-size: 18px;
+
+  &:hover {
+    background-color: #3c3c3c;
+  }
 `
--- src/renderer/src/components/styled/Input.jsx
+++ src/renderer/src/components/styled/Input.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const Input = styled.input`
-  padding: 10px;
-  border: 2px solid skyblue;
+  padding: 5px 10px;
+  font-size: 18px;
   outline: 0;
+  border-radius: none;
 `
